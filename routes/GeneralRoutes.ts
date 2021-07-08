c72e33d5ade791ffa0f441b3009a286afb877310
Jonathan Fritz <fritz@nosc.io>
2021-07-08 13:46:46 +0200

    Deprecating getvalidation endponit in favor of getvalidations endpoint

--- routes/GeneralRoutes.ts
+++ routes/GeneralRoutes.ts
@@ -17,35 +17,6 @@ const gwfManager = new GreenWebFoundationFetcher();
 const validationManager = ValidationManager.instance;
 
 /**
- * Get the validation for the key
- * @returns validation
- */
-async function getValidation(key): Promise<any> {
-	if (key) {
-		const redisResult = await validationManager.getLinkInformation(key);
-		// Success, key was cached and is available
-		if (redisResult) {
-			return {
-				validation: JSON.parse(redisResult)
-			}
-		} else {
-			// const validationData = await validationManager.getLinkInformation(key);
-			// console.log(validationData);
-			// redisManager.setCache(key, validationData);
-
-			// TODO: Add validation for unknown sites
-			return {
-				validation: {
-					isGreen: 0,
-				}
-			}
-		}
-	} else {
-		return null;
-	}
-}
-
-/**
  * Gets the system status
  */
 generalRouter.get('/status', async (req, res) => {
@@ -65,40 +36,8 @@ generalRouter.get('/updateDatabase', async (req, res) => {
 });
 
 /**
- * Gets the websites validation
+ * Returns the website validation
  */
-generalRouter.post('/getValidation', async (req, res) => {
-	const key: string = req?.body?.key;
-	if (key) {
-		// const redisResult: string = await redisManager.checkCache(key);
-		const redisResult = await validationManager.getLinkInformation(key);
-		// Success, key was cached and is available
-		if (redisResult) {
-			res.send({
-				success: true,
-				message: "Cached",
-				validation: JSON.parse(redisResult)
-			})
-		} else { // Not available yet
-			// const validationData = await validationManager.getLinkInformation(key);
-			// console.log(validationData);
-			// redisManager.setCache(key, validationData);
-			res.send({
-				success: true,
-				message: "Not cached",
-				validation: {
-					isGreen: 0,
-				}
-			})
-		}
-	} else {
-		res.send({
-			success: false,
-			message: "Cannot find the key in your body. Check your request!"
-		})
-	}
-});
-
 generalRouter.post('/getValidations', async (req, res) => {
 	const keys: string[] = req?.body?.keys;
 
