9314523524f8a0b0c4e43849f86f7ba820c68bf1
Jonathan Fritz <fritz@nosc.io>
2021-07-08 13:32:38 +0200

    Deprecating getvalidation endponit in favor of getvalidations endpoint

--- routes/GeneralRoutes.ts
+++ routes/GeneralRoutes.ts
@@ -99,7 +99,47 @@ generalRouter.post('/getValidation', async (req, res) => {
 	}
 });
 
+generalRouter.post('/getValidations', async (req, res) => {
+	const keys: string[] = req?.body?.keys;
 
+	let results = [];
+	if (keys) {
+		for (var keyRef in keys) {
+			const key = keys[keyRef]
+			console.log("Looking at key: " + key)
+			if (key) {
+				// const redisResult: string = await redisManager.checkCache(key);
+				const redisResult = await validationManager.getLinkInformation(key);
+				// Success, key was cached and is available
+				if (redisResult) {
+					results.push(
+						{ validation: JSON.parse(redisResult) }
+					)
+				} else { // Not available yet
+					// const validationData = await validationManager.getLinkInformation(key);
+					// console.log(validationData);
+					// redisManager.setCache(key, validationData);
+					results.push(
+						{
+							validation: {
+								isGreen: 0,
+							}
+						}
+					)
+				}
+			}
+		}
+		return res.send({
+			success: true,
+			validations: results
+		})
+	} else {
+		return res.send({
+			success: false,
+			message: "Keys not found in your request"
+		})
+	}
+});
 
 
 
