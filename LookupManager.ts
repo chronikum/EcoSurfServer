d7714e0ced1bcdfcbd6fc35eda3f05f7bfc30c09
Jonathan Fritz <fritz@nosc.io>
2021-07-10 18:51:08 +0200

    Lookup many at once

--- LookupManager.ts
+++ LookupManager.ts
@@ -49,6 +49,7 @@ export default class LookUpManager {
 	 */
 	async checkMultiCache(keys: String[]): Promise<Validation[]> {
 		var regexExp = new RegExp(keys.join("|"), 'gi') as unknown as string;
+		regexExp = "^" + regexExp;
 		const matches = await ValidationModel.find({
 			f: { $regex: regexExp, $options: 'm' },
 		}) as unknown as Validation[];
